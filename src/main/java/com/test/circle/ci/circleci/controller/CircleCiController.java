083ba7ff80b97e7974fc121805d254863585125f
Madhavankarthik <madhavankarthi2010@gmail.com>
2020-03-24 21:37:17 +0530

    simple change

---
+++ src/main/java/com/test/circle/ci/circleci/controller/CircleCiController.java
@@ -0,0 +1,13 @@
+package com.test.circle.ci.circleci.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CircleCiController {
+
+    @GetMapping("\hello-world")
+    public String testHelloWorld(){
+        return "Hello World";
+    }
+}
