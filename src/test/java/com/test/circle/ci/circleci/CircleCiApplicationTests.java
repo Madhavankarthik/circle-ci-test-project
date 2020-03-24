8002c91ae0bd86d71c1bcfcdd2f9aebd3c07feaa
Madhavankarthik <madhavankarthi2010@gmail.com>
2020-03-24 22:11:11 +0530

    comit test failure

--- src/test/java/com/test/circle/ci/circleci/CircleCiApplicationTests.java
+++ src/test/java/com/test/circle/ci/circleci/CircleCiApplicationTests.java
@@ -3,11 +3,14 @@ package com.test.circle.ci.circleci;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CircleCiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals("test","tester");
 	}
 
 }
